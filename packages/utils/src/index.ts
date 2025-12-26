d9fa23041dfbd5595e7e4d67ae25af1d249ff541
Joanna <sjy9280@163.com>
2025-12-26 17:51:58 +0800

    update

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,6 +1,6 @@
 export * from './global';
+export * from './browser';
 export * from './tools';
 export * from './http';
 export * from './queue';
 export * from './logger';
-export * from './browser'
